1caa9fbf6be67a5131b22b623cc2e4109a3d7ce8
gvtek0 <gvtek0@gmail.com>
2020-06-14 13:32:49 -0700

    prompts w/ date

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace dotnet_helloworld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} ad {date:t}!");
+            Console.Write("\nPress any key to exit....");
+            Console.ReadKey(true);
         }
     }
 }
